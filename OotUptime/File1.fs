051930d484e5a39e928eaf24feb06c2a6abf8a91
ootthuen <oothuen@gmail.com>
2015-12-29 09:56:45 +0100

    2

--- OotUptime/File1.fs
+++ OotUptime/File1.fs
@@ -19,31 +19,39 @@ let configure (dataContext : ClientBase<'T>) address isHttps =
     //do client.DataContext.ClientCredentials.UserName.UserName <- "Username"
     //do client.DataContext.ClientCredentials.UserName.Password <- "Password"
     dataContext.Endpoint.Binding <- binding isHttps
+    
+type Test = {name: string; execution: unit -> unit}
+type TestResult = {name: string; success: bool; message: Option<string>}
 
-let tryExecute m s = async{
+let tryExecute (t:Test) = async{
     try
-        m() |> ignore
-        Console.WriteLine(s + "\tSUCCESS")
+        t.execution()
+        return {name=t.name; success=true; message=None}
     with
         | ex -> 
-            Console.WriteLine(s + "\tFAIL")
+            return {name=t.name; success=false; message=Some(ex.ToString())}
     }
 
-let tests = new List<Async<unit>>()
+let tests = new List<Test>()
 
 type Service1 = WsdlService<ServiceUri="https://localhost:4711/MT_1_v1.svc?wsdl">    
 let client1 = Service1.GetBasicHttpsBinding_ITimeService()
 configure client1.DataContext "https://localhost:4711/MT_1_v1.svc" true
-let c1 = tryExecute client1.GetTime "https"
+let c1 = {name="https"; execution=(fun()->client1.GetTime()|>ignore)}
 tests.Add(c1)
 
 type Service2 = WsdlService<ServiceUri="http://localhost:4710/MT_1_v1.svc?wsdl">    
 let client2 = Service2.GetBasicHttpBinding_ITimeService()
-configure client2.DataContext "http://localhost:4710/MT_1_v1.svc" false
-let c2 = tryExecute client2.GetTime "http"
+configure client2.DataContext "http://localhost:4710/MT_1_v12.svc" false
+let c2 = {name="http"; execution=(fun()->client2.GetTime()|>ignore)}
 tests.Add(c2)
 
 tests
+    |> Seq.map (fun (test) -> tryExecute test)
     |> Async.Parallel
     |> Async.RunSynchronously
+    |> Seq.iter (
+        fun (testResult) -> 
+            let suc = if testResult.success then "Success" else "Fail" 
+            Console.WriteLine(testResult.name + ":\t" + suc))
     |> ignore
